a868e6bf0d9bb815a1f8e73b6d3137f3082ba8a1
wpseemol <wpseemol@gmail.com>
2024-12-11 17:06:16 +0600

    theme providers add on my site

---
+++ src/components/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { ThemeProvider } from './ui/theme-provider';
+
+export default function Providers({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="light"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+        </>
+    );
+}
