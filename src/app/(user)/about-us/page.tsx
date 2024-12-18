1cea314c39fc66e156d7760273ee10314194bfbd
wpseemol <wpseemol@gmail.com>
2024-12-19 01:02:19 +0600

    text color change about page header

--- src/app/(user)/about-us/page.tsx
+++ src/app/(user)/about-us/page.tsx
@@ -9,7 +9,11 @@ export default function AboutUsPage() {
             <section className="space-y-5 mb-14 w-[75%] mx-auto">
                 <div className="text-center">
                     <h2 className="text-neutral-900/85 dark:text-white sm:text-[52px] text-3xl font-bold leading-snug text-center mb-3">
-                        Our<span className="text-primary"> Story</span>
+                        Our
+                        <span className="text-primary dark:text-white">
+                            {' '}
+                            Story
+                        </span>
                     </h2>
 
                     <p className="text-center">
