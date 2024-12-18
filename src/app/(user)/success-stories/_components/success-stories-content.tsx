0ca7d72f515e6ed2a10c53c23c162cbcc69d5961
wpseemol <wpseemol@gmail.com>
2024-12-19 00:14:05 +0600

    our success stories content section card desigine change

--- src/app/(user)/success-stories/_components/success-stories-content.tsx
+++ src/app/(user)/success-stories/_components/success-stories-content.tsx
@@ -1,11 +1,14 @@
-import { successStories } from '@/lib/db/success-stories';
-import SuccessStoriesCard from './success-stories-card';
+import { testimonials } from '@/lib/db/testimonial-data';
+import SuccessStoriesTestimonialsCard from './success-stories-testmonial-card';
 
 export default function SuccessStoriesContent() {
     return (
         <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
-            {successStories.map((stories) => (
-                <SuccessStoriesCard storiesDetails={stories} key={stories.id} />
+            {testimonials.map((stories) => (
+                <SuccessStoriesTestimonialsCard
+                    content={stories}
+                    key={stories.id}
+                />
             ))}
         </div>
     );
