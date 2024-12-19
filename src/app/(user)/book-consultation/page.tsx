3721930e1a5056a571fd02f9ee4e5e90921bf028
wpseemol <wpseemol@gmail.com>
2024-12-19 21:17:57 +0600

    done

--- src/app/(user)/book-consultation/page.tsx
+++ src/app/(user)/book-consultation/page.tsx
@@ -1,10 +1,15 @@
 export default function BookConsultationPage() {
     return (
         <main>
-            <section>
-                <h2>
+            <section className="container mx-auto h-[25rem] flex justify-center items-center">
+                <h2 className="text-2xl text-center font-medium text-neutral-900/85 dark:text-neutral-100/90 drop-shadow-lg">
                     You will be able to book online appointment with us from
-                    23rd Dec 2024. Stay tuned!
+                    <span className="text-primary dark:text-blue-600">
+                        {' '}
+                        23rd Dec 2024
+                    </span>
+                    .
+                    <br /> Stay tuned!
                 </h2>
             </section>
         </main>
