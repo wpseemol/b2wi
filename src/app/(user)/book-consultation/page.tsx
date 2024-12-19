b6faebbb0aaaa008cd19e1797693030ef05f67d3
wpseemol <wpseemol@gmail.com>
2024-12-19 21:04:30 +0600

    book bonsalanchi

---
+++ src/app/(user)/book-consultation/page.tsx
@@ -0,0 +1,12 @@
+export default function BookConsultationPage() {
+    return (
+        <main>
+            <section>
+                <h2>
+                    You will be able to book online appointment with us from
+                    23rd Dec 2024. Stay tuned!
+                </h2>
+            </section>
+        </main>
+    );
+}
